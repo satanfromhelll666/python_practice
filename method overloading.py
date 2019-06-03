054a85dced860400dbc22f20b12e56935d67695b
satanfromhelll666 <mkhan172175@bscse.uiu.ac.bd>
2019-06-04 04:15:46 +0600

    method overriding

---
+++ method overloading.py
@@ -0,0 +1,28 @@
+class a:
+    def mobile():
+        print('this is class a mobile')
+
+class b(a):
+    pass
+
+
+
+mob1 = b
+
+mob1.mobile()#here b inharite a this is why it print this is class a mobile
+
+
+class c:
+    def mobile():
+        print('this is class c mobile')
+
+class d(c):
+    def mobile():
+        print('this is class d mobile')
+
+mob2 = d
+
+mob2.mobile()
+
+
+
