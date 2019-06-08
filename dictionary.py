a2501b3c9b4635cbf74946087dbafab0cfdd369d
satanfromhelll666 <mkhan172175@bscse.uiu.ac.bd>
2019-06-09 01:28:51 +0600

    book

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+states = {
+'Oregon': 'OR',
+'Florida': 'FL',
+'California': 'CA',
+'New York': 'NY',
+'Michigan': 'MI'
+}
+
+states["Dhaka"] = 'DH'
+
+print(states['Oregon'])
+
+print(states)
+
+del states['California']
+
+print(states)
