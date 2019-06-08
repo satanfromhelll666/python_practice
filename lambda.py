fbe63aa424bcbb07a201b02184d8095d084d4c36
satanfromhelll666 <mkhan172175@bscse.uiu.ac.bd>
2019-06-09 03:56:33 +0600

    nice things

---
+++ lambda.py
@@ -0,0 +1,12 @@
+#lambda is like a anonymous function
+
+a = lambda x,y:(x+2)+(y+2) # in fucntion we write def(x,y) here lambda x,y doing the same job 
+
+# after write ':' we can write the logic here the logic is (x+2)+(y+2)
+
+# here a is like the function name  
+
+print(a(2,2))
+
+
+
