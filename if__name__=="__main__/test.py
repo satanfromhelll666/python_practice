9e2f0bea51c5f704f69cb263c124883c36516f45
satanfromhelll666 <mkhan172175@bscse.uiu.ac.bd>
2019-06-11 03:27:37 +0600

    this is module_tester

---
+++ if__name__=="__main__/test.py
@@ -0,0 +1,22 @@
+
+from module_test import *
+
+
+def one():
+    print('this is test 1')
+
+def two():
+    print('this is test 2')
+    
+def main():
+    one()
+    two()
+
+
+main()
+    
+
+
+
+
+
