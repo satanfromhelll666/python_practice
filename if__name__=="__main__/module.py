bb727cb51773b35b5ba5311a271e406612727754
satanfromhelll666 <mkhan172175@bscse.uiu.ac.bd>
2019-06-11 03:27:07 +0600

    this is module

---
+++ if__name__=="__main__/module.py
@@ -0,0 +1,16 @@
+
+def three():
+    
+    print('this is module 3')
+
+def four():
+    print('this is module 4')
+
+def main():
+    three()
+    four()
+
+if __name__ == '__main__':
+    main()
+    
+
