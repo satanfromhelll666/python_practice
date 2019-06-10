dd43ed09d5315883237bd57d5a5136f461839086
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2019-06-11 03:46:59 +0600

    Update test.py

--- if__name__=="__main__/test.py
+++ if__name__=="__main__/test.py
@@ -1,9 +1,14 @@
 
-from module_test import *
+from module_test import * 
+#when i import a module if there are any function call in the module it will call automaticly ....
+#for thats why we use __name__== "__main__" .... if a code run from the main script ..
+#the main script ( __name__) variable always return the __main__
+#if a code run from the main script and the import script (__name__) variable will return the name of the script   
 
 
 def one():
     print('this is test 1')
+    three()
 
 def two():
     print('this is test 2')
