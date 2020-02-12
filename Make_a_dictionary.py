055af848ec4fa8785f6f02bc534bb998a220c276
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-02-12 15:14:26 +0600

    Update Make_a_dictionary.py

--- Make_a_dictionary.py
+++ Make_a_dictionary.py
@@ -1,1 +1,10 @@
 
+pupils_dictionary = {}
+
+for x in range(3):    
+    new_key = input('Enter new key: ')
+    new_age = input('Enter new age: ')
+    pupils_dictionary[new_key] = new_age
+  
+  
+print(pupils_dictionary)
